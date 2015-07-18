ea245b495ab419dc7c6588a97c29f44c3e68c01b
Samuel Hapák <samuel.hapak@gmail.com>
2015-07-18 16:45:00 +0200

    Fix gulp tasks.
    Fix .eslintrc rules. Remove unnecessary packages from package.json.
    Use harmonize so jest works.

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,54 @@
 /* eslint-env node */
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var sourcemaps = require("gulp-sourcemaps");
-var babel = require("gulp-babel");
-var source = require("vinyl-source-stream");
-var buffer = require("vinyl-buffer");
-var jest = require("jest-cli");
-var eslint = require("gulp-eslint");
-var runSequence = require("run-sequence");
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var jest = require('jest-cli');
+var eslint = require('gulp-eslint');
+var runSequence = require('run-sequence');
 
-gulp.task("build", function() {
-  return gulp.src("src/**/*.js")
+require('harmonize')();
+
+gulp.task('build', function() {
+  return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel({stage: 0}))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("dist"));
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task("jest", function(done) {
-  var rootDir = "./src";
+gulp.task('jest', function(done) {
+  var rootDir = './src';
   jest.runCLI({config: {
-    "rootDir": rootDir,
-    "scriptPreprocessor": "../node_modules/babel-jest",
-    "testFileExtensions": ["es6", "js"],
-    "moduleFileExtensions": ["js", "json", "es6"]
+    'rootDir': rootDir,
+    'scriptPreprocessor': '../node_modules/babel-jest',
+    'testFileExtensions': ['es6', 'js'],
+    'moduleFileExtensions': ['js', 'json', 'es6']
   }}, rootDir, function(success) {
     /* eslint no-process-exit:0 */
-    done(success ? null : "jest failed");
-    process.on("exit", function() {
+    done(success ? null : 'jest failed');
+    process.on('exit', function() {
       process.exit(success ? 0 : 1);
     });
   });
 });
 
-gulp.task("eslint", function() {
+gulp.task('eslint', function() {
   return gulp.src([
-      "gulpfile.js",
-      "src/**/*.js"
+      'gulpfile.js',
+      'src/**/*.js'
     ])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
 });
 
-gulp.task("test", function(done) {
+gulp.task('test', function(done) {
   // Run test tasks serially, because it doesn't make sense to build when tests
   // are not passing, and it doesn't make sense to run tests, if lint has failed.
   // Gulp deps aren't helpful, because we want to run tasks without deps as well.
-  runSequence("eslint", "jest", done);
+  runSequence('eslint', 'jest', done);
 });
 
-gulp.task("default", ["build"]);
+gulp.task('default', ['build']);
